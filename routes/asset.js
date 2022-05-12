c2d6bf40a1e9fa9b143b24d60bd600bf653d6019
Hala Assi <92643585+halaassi@users.noreply.github.com>
2022-05-12 18:21:23 +0300

    add asset api

---
+++ routes/asset.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const session = require('express-session');
+const router = express.Router();
+const flash = require('connect-flash');
+const users = require('../models/userModel');
+const asset = require('../models/assetModel');
+const Joi = require('joi')
+const crypto = require('crypto');
+
+
+//-----------------asset add page-----------------------------------------------------
+router.get('/addasset', (req, res, next) => {
+    const messages = req.flash();
+    res.render('addasset', { messages });
+});
+
+
+router.get('/assetadmin', (req, res, next) => {
+  const messages = req.flash();
+  res.render('assetadmin', { messages });
+});
+
+
+router.post('/addasset', (req, res, next) => {
+  const assetParams = req.body;
+ 
+  const payload = {
+
+    productId:  assetParams.productId,
+    productName:  assetParams.productName,
+    productCategory: assetParams.productCategory
+
+  };
+
+  asset.insertMany(payload, (err) => {
+    if (err) {
+      req.flash('error', ' Product Id already exists.');
+      res.redirect('/asset/addasset');
+    } 
+    else {
+      req.flash('success', ' Assets have been successfully added.');
+      sess = req.session;
+      sess.productId = req.body.productId;
+      res.redirect('/assetpage');
+   
+    }
+    
+  });
+});
+module.exports = router;
