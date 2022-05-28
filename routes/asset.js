582ef0807e380d5f0c605dd84fce13687682caf1
Hala Assi <92643585+halaassi@users.noreply.github.com>
2022-05-28 21:52:07 +0300

    Updete  2 asset .js

--- routes/asset.js
+++ routes/asset.js
@@ -43,76 +43,44 @@ router.post('/addasset', (req, res, next) => {
   });
     
 });
-
-router.get('/useraddasset/:productId', async(req, res) => {
-
-
-  const id = req.params.productId;  
-  
-  sess = req.session;
-  email=sess.email;
-
-  assets.findOneAndUpdate({productId: id} ,{$set: {'user_email': email}}, function(err, results) {
-    
-    if (err ) {
-      console.log(err);
-      req.flash('error', 'error occure.');
-      res.redirect('/userassetpage');
-    } 
-    else {
-     
-      req.flash('success', ' Assets have been successfully added');
-      console.log('success');
-   
+router.get('/back',(req,res) => {
+ 
+  req.session.destroy((err) => {
+      if(err) {
+          return console.log(err);
+      }
       if(email.includes('admin')){
-        sess = req.session;
-        email=sess.email;
-      
         res.redirect('/assetpage');
       }
       else{
         sess = req.session;
         email=sess.email;
-      
         res.redirect('/userassetpage');
       }
-    }
    
-  })
-   
-});
-
-router.get('/userdeleteasset/:productId', function(req, res, next) {
-  
-  const id = req.params.productId;
-  assets.findOneAndUpdate({productId: id} ,{$set: {'user_email':null}}, function(err, results) {
-    
-    if (err ) {
-      console.log(err);
-      req.flash('error', 'error occure.');
-      res.redirect('/myasset');
-    } 
-    else {
-      sess = req.session;
-      email=sess.email;
-
-      req.flash('success', ' Assets have been successfully added');
-      console.log('success');
-   
-      res.redirect('/myasset');
-    }
-   
-  })
-})
-
-router.get('/back',(req,res) => {
-  req.session.destroy((err) => {
-      if(err) {
-          return console.log(err);
-      }
-      res.redirect('/assetpage');
   });
 
 });
+router.get('/delete/:productId', function(req, res, next) {
+
+  const id = req.params.productId;
+    assets.findOneAndDelete({productId: id} , function(err,data) {
+
+
+      if (err) {
+     
+          console.log('Failed to Delete asset Details: ' + err);
+
+
+      } else {
+        sess = req.session;
+        email=sess.email;
+      
+        res.redirect('/assetpage');
+   
+      }
+  });
+})
+
 
 module.exports = router;
