945f8eb8e8f47837b6f408d28a14d62cd40d8f3a
RaghadBadawi <92644475+RaghadBadawi@users.noreply.github.com>
2022-05-28 21:54:07 +0300

    update myasset.js

---
+++ routes/myasset.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const session = require('express-session');
+const asset = require('../models/assetModel');
+const crypto = require('crypto');
+
+
+
+
+router.get('/', (req, res)=> {
+ 
+    sess = req.session;
+    email=sess.email;
+
+   asset.find({ user_email: email },(err, docs) => {
+       if (!err) {
+           res.render('Myasset', {
+            list: docs
+           });
+       } else {
+           console.log('Failed to retrieve the Course List: ' + err);
+       }
+   });
+
+});
+
+
+  // --------------------------------------------------
+module.exports=router;
